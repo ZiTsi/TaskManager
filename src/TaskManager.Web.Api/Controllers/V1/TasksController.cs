2312b1cc4899dfc1cc63cecdde715cd4cd327f9e
ZiTsi <zitsiou@gmail.com>
2015-09-18 17:35:06 +0300

    Until page 68

--- src/TaskManager.Web.Api/Controllers/V1/TasksController.cs
+++ src/TaskManager.Web.Api/Controllers/V1/TasksController.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web.Http;
+using TaskManager.Web.Api.Models;
+using TaskManager.Web.Common.Routing;
 
 namespace TaskManager.Web.Api.Controllers.V1
 {
+    [ApiVersion1RoutePrefix("tasks")]
     public class TasksController : ApiController
     {
+        [Route("", Name = "AddTaskRoute")]
+        [HttpPost]
+        public Task AddTask(HttpRequestMessage requestMessage, Task newTask)
+        {            
+            if(!(newTask == null))          
+                return new Task { Subject = "In v1, newTask.Subject = " + newTask.Subject };
+            else            
+                return new Task { Subject = "In v1, not provided newTask.Subject"};           
+        }
     }
 }
--- src/TaskManager.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
+++ src/TaskManager.Web.Common/Routing/ApiVersion1RoutePrefixAttribute.cs
@@ -12,5 +12,3 @@ namespace TaskManager.Web.Common.Routing
         }
     }
 }
-
-telos 61
